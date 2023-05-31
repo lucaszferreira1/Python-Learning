3a71dece0ed07ab3061c2ccdf9b10233dc259748
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2023-05-31 16:04:44 -0300

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -1,29 +1,77 @@
 import pygame
 
+def getDefaultRect(x, y):
+    return pygame.Rect(x * scaleX, y * scaleY, scaleX, scaleY)
+
+def drawRect(x, y, color = (25, 25, 25)):
+    rect = getDefaultRect(x, y)
+    pygame.draw.rect(screen, color, rect)
+
+def BFS():
+    add = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+    visited = []
+    queue = [start]
+    while(queue):
+        no = queue.pop(0)
+        visited.append(no)
+        for i in add:
+            nodeCheck = [no[0] + i[0], no[1] + i[1]]
+            if nodeCheck[0] < 0 or nodeCheck[1] < 0:
+                continue
+            verify = allSquares[nodeCheck[0]][nodeCheck[1]]
+            if nodeCheck == goal:
+                return visited
+            if verify == free and nodeCheck not in visited:
+                queue.append(nodeCheck)
+                visited.append(nodeCheck)
+
+
 if __name__ == "__main__":
     
     pygame.init()
-    width = 720
-    height = 720
+    width = 820
+    height = 820
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
     running = True
 
-    allSquares = [[0,0,0,0],
-                  [0,1,1,0],
-                  [0,1,1,0],
-                  [0,0,0,0],]
+    allSquares = ["  ###################",
+                  "      #     #   #   #",
+                  "### ### # ### # # # #",
+                  "# #   # #     #   # #",
+                  "# ### ### ### ##### #",
+                  "#   #   #   # #     #",
+                  "# # # ####### ### ###",
+                  "# #   # #   # #     #",
+                  "##### # # # # # #####",
+                  "# #       #   #     #",
+                  "# # ##### # #########",
+                  "#   #     #         #",
+                  "# # # ### ###########",
+                  "# # #   # #     #   #",
+                  "####### # # ### ### #",
+                  "# # # # #     # # # #",
+                  "# # # ####### ### # #",
+                  "# # #   # # #   #   #",
+                  "# # # # # # # ### # #",
+                  "#     #           #  ",
+                  "###################  "]
 
-    scaleX = width / len(allSquares)
-    scaleY = height / len(allSquares[0])
+    scaleX = width / len(allSquares[0])
+    scaleY = height / len(allSquares)
 
     start = [0, 0]
-    startRect = pygame.Rect(start[0] * scaleX, start[1] * scaleY, scaleX, scaleY)
+    startRect = getDefaultRect(start[0], start[1])
+    
+    goal = [len(allSquares) - 1, len(allSquares[:-1]) - 1]
+    goalRect = getDefaultRect(goal[0], goal[1])
 
-    goal = [3, 3]
-    goalRect = pygame.Rect(goal[0] * scaleX, goal[1] * scaleY, scaleX, scaleY)
-
-
+    wall = '#'
+    free = ' '
+    
+    visited = BFS()
+    lastDrawn = 0
+    print(visited)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -33,15 +81,21 @@ if __name__ == "__main__":
 
         for i in range(len(allSquares)):
             for j in range(len(allSquares[i])):
-                if allSquares[i][j]:
-                    pygame.draw.rect(screen, (100, 100, 100), (i * scaleX, j * scaleY, scaleX, scaleY))
+                if allSquares[i][j] == wall:
+                    pygame.draw.rect(screen, (100, 100, 100), (j * scaleX, i * scaleY, scaleX, scaleY))
 
         pygame.draw.rect(screen, (30, 30, 150), startRect)
         pygame.draw.rect(screen, (150, 30, 30), goalRect)
-
+        
         pygame.display.flip()
 
-        clock.tick(60)
+        for i in range(0, lastDrawn):
+            draw = visited[i]
+            drawRect(draw[0], draw[1], (0, 130, 0))
+            lastDrawn += 1
+        
+
+        clock.tick(10)
 
     pygame.quit()
     
