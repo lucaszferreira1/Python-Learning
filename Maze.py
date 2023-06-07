3389c19c5b3b0b3018195126203ea2abce3c0e56
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2023-06-07 15:08:02 -0300

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -137,6 +137,7 @@ if __name__ == "__main__":
     
     # vis = maze_with_dfs(50)
 
+    lastDrawn = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
